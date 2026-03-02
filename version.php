ee325ab4a2a699323152dbc2bb8c1e6947dcce20
Lyelfiz <Lyelfiz@gmail.com>
2026-03-02 09:34:19 -0400

    Update version.php

--- version.php
+++ version.php
@@ -4,8 +4,8 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2026022700;
+$plugin->version = 2026030200;
 $plugin->requires = 2024041900;
 $plugin->component = 'auth_fieldcpflogin';
-$plugin->release = '1.0.1';
+$plugin->release = '1.0.2';
 $plugin->maturity = MATURITY_STABLE;
